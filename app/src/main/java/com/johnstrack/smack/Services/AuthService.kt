b5519f00ebdaabd390f5a9f8efc8c42e14e13baa
John Strack <Braveheart22@GMail.com>
2018-04-16 17:07:47 -0400

    Add constants, and authService files

---
+++ app/src/main/java/com/johnstrack/smack/Services/AuthService.kt
@@ -0,0 +1,9 @@
+package com.johnstrack.smack.Services
+
+/**
+ * Created by John on 4/16/2018 at 4:57 PM.
+ */
+object AuthService {
+
+
+}
---
+++ app/src/main/java/com/johnstrack/smack/Utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.johnstrack.smack.Utilities
+
+/**
+ * Created by John on 4/16/2018 at 4:58 PM.
+ */
+
+const val BASE_URL = "https://slack-ii.herokuapp.com/v1/"
+const val URL_REGISTER = "${BASE_URL}account/register"
