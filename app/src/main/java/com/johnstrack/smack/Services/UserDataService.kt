53e8c0c901c6d8c522a6a810c62174895e6928de
John Strack <Braveheart22@GMail.com>
2018-04-19 13:36:52 -0400

    Add create user functionality.

---
+++ app/src/main/java/com/johnstrack/smack/Services/UserDataService.kt
@@ -0,0 +1,14 @@
+package com.johnstrack.smack.Services
+
+/**
+ * Created by John on 4/19/2018 at 1:21 PM.
+ */
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+}
--- app/src/main/java/com/johnstrack/smack/Utilities/Constants.kt
+++ app/src/main/java/com/johnstrack/smack/Utilities/Constants.kt
@@ -7,3 +7,4 @@ package com.johnstrack.smack.Utilities
 const val BASE_URL = "https://slack-ii.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
