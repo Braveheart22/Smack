656e720113ad2e9779de5716a649ebf0b3c6e23e
John Strack <Braveheart22@GMail.com>
2018-04-23 11:53:53 -0400

    Login user upon creating the user

--- app/src/main/java/com/johnstrack/smack/Services/UserDataService.kt
+++ app/src/main/java/com/johnstrack/smack/Services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.johnstrack.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 /**
  * Created by John on 4/19/2018 at 1:21 PM.
  */
@@ -11,4 +14,23 @@ object UserDataService {
     var email = ""
     var name = ""
 
+    fun returnAvatarColor (components: String): Int {
+
+        val strippedColor = components.replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner (strippedColor)
+        if (scanner.hasNext()) {
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(r, g, b)
+    }
 }
